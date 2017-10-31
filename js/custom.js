134256ba5bd1e71b75bf904f1ee108778724f4ef
localst <32475536+localst@users.noreply.github.com>
2017-10-31 18:56:09 +1100

    Pre Launch testing

--- js/custom.js
+++ js/custom.js
@@ -13,15 +13,19 @@ $(function(){
 
 // Scroll to Respective Section between Pages
 var headerHeight = $('.logo-nav').height();
-$('a.scrollTo').on('click',function(e) {
+$(document).on('click','a.scrollTo',function(e) {
 
 	var target = this.hash;
 	var $target = $(target);
 	var targetname = target.slice(1, target.length);
-	console.log($('.logo-nav').height());
+
 	if(document.getElementById(targetname) != null) {
+
 		e.preventDefault();
 		target_offset = $target.offset() ? $target.offset().top : 0;
+
+		$('.offcanvas').removeClass('show-offcanvas');
+		$('.body-inactive').fadeOut(350);
 
 		$('html, body').stop().animate({
 			'scrollTop': target_offset - headerHeight
@@ -35,9 +39,9 @@ $('a.scrollTo').on('click',function(e) {
 
 $(window).load(function(){
 	hash = window.location.hash;
-		$('html, body').stop().animate({
-			'scrollTop': $(hash).offset().top - headerHeight
-		},500);	
+	$('html, body').stop().animate({
+		'scrollTop': $(hash).offset().top - headerHeight
+	},500);	
 })
 
 
@@ -52,7 +56,29 @@ $(window).scroll(function(){
 	else{
 		$('.logo-nav').removeClass('sticky-logo-nav');
 	}
-})
+});
+
+
+// Mobile Menu
+
+	// cloning menu items in mobile
+	$mobileNav = $('<div id="mobileNav"></div>').prependTo('.offcanvas .nano .nano-content');
+	$mobileNav.append($('.header ul.mainNav').clone());
+
+	// offcanvas toggle
+	$('.toggle a').click(function(e){
+		$('.offcanvas').addClass('show-offcanvas');
+		$('body').addClass('pushed');
+		$('.body-inactive').fadeIn(350);
+		e.preventDefault();
+	});
+
+	// closing ups clicking on the screen
+	$('.body-inactive, .coff a').click(function(e){
+		$('.offcanvas').removeClass('show-offcanvas');
+		$('.body-inactive').fadeOut(350);
+		e.preventDefault();
+	});
 
 	// Window Resize Orientation Change
 	$(window).bind('resize orientationchange',function(){
